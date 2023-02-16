c4a7130ffee91e640a8c565df10bc81105d23962
4RR6R <109877693+LuckyScripters@users.noreply.github.com>
2023-02-16 12:18:07 +0100

    Update ip.js

--- ip.js
+++ ip.js
@@ -7,6 +7,7 @@ NewXMLHttpRequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         fs.writeFile("ip.json", `{"IP" : "${this.responseText}"}`, function(error) {
             if (error == null) {
+                alert(`Your IP is ${this.responseText}`)
                 return
             }
             console.log(error)
