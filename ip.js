3fc334cb7e226b846484cfff54298a3491ed7570
4RR6R <109877693+LuckyScripters@users.noreply.github.com>
2023-03-05 01:46:52 +0100

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,21 +1,22 @@
-import fs from "fs"
-import XMLHttpRequest "xhr2"
+import http from "http"
+import fetch from "node-fetch"
 
-const NewXMLHttpRequest = new XMLHttpRequest();
-
-NewXMLHttpRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        fs.writeFile("ip.json", `{"IP" : "${this.responseText}"}`, function(error) {
-            if (error == null) {
-                alert(`Your IP is ${this.responseText}`)
-                return
-            }
-            console.log(error)
-        })
-    }
-};
-
-function SendRequest() {
-    NewXMLHttpRequest.open("GET", "https://api.ipify.org/", true);
-    NewXMLHttpRequest.send();
+function handleRequest(request, response) {
+  if (request.method === "GET" && request.url === "/") {
+    fetch("https://www.roblox.com/my/settings/json", {
+      method : "GET"
+    })
+     .then((data) => {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/plain");
+        response.write(data);
+        response.end();
+    })
+  }
 }
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
